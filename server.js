00c2d3d4b6493169aa6847e95c6cc77bbfc947ac
Tina Nguyen <nguyentina2005@yahoo.com>
2017-07-29 14:37:12 -0400

    fix htmlRoutes

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-//Users information(DATA)
-var friends = [{
-    
-}]
+
 /* Basic route that sends the user first to the AJAX Page from the htmlRoutes. The get to show the html and css on the website*/
 require("./app/routing/htmlRoutes.js");
 
