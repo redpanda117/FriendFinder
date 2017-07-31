280c2212a9838a623a59c2d0ebcc9df14219d4cc
Tina Nguyen <nguyentina2005@yahoo.com>
2017-07-30 20:08:35 -0400

    use input is push to array
    user input is push to the array. $.post is working, but input not permintly save when sever restart the user input data is gone.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,19 @@
 //data of all friends
-var friends =[{}];
+var friends = [{
+    "name": "Monica",
+    "photo": "https://images.moviepilot.com/images/c_limit,q_auto:good,w_600/courteney-cox-courteney-cox-33143449-1600-1200-10-years-later-still-friends-where-are-monica-phoebe-joey-rachel-ross-chandler-now-jpeg-122716/10-years-later-still-friends-where-are-monica-phoebe-joey-rachel-ross-chandler-now.jpg",
+    "score":[
+      5,
+      3,
+      4,
+      2,
+      2,
+      1,
+      2,
+      3,
+      3,
+      5
+    ]
+}];
 
 module.exports = friends;
