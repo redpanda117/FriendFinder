0b734a025254d0e9e51fa24a710f7dfa78fe0152
Tina Nguyen <nguyentina2005@yahoo.com>
2017-07-31 19:50:42 -0400

    validation and difference calculated
    validate that user provided an answer n all fields. Able to calculate the diference between user and friends. Fix spelling and added more firends to the friends array.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,64 @@
 //data of all friends
 var friends = [{
-    "name": "Monica",
-    "photo": "https://images.moviepilot.com/images/c_limit,q_auto:good,w_600/courteney-cox-courteney-cox-33143449-1600-1200-10-years-later-still-friends-where-are-monica-phoebe-joey-rachel-ross-chandler-now-jpeg-122716/10-years-later-still-friends-where-are-monica-phoebe-joey-rachel-ross-chandler-now.jpg",
-    "score":[
-      5,
-      3,
-      4,
-      2,
-      2,
-      1,
-      2,
-      3,
-      3,
-      5
-    ]
-}];
+        "name": "Monica",
+        "photo": "https://images.moviepilot.com/images/c_limit,q_auto:good,w_600/courteney-cox-courteney-cox-33143449-1600-1200-10-years-later-still-friends-where-are-monica-phoebe-joey-rachel-ross-chandler-now-jpeg-122716/10-years-later-still-friends-where-are-monica-phoebe-joey-rachel-ross-chandler-now.jpg",
+        "scores": [
+            5,
+            3,
+            4,
+            2,
+            2,
+            1,
+            2,
+            3,
+            3,
+            5
+        ]
+},{
+        "name": "Phoebe",
+        "photo": "https://tmusciano.files.wordpress.com/2013/01/phoeboe.jpg",
+        "scores": [
+            2,
+            5,
+            4,
+            5,
+            3,
+            5,
+            4,
+            2,
+            1
+        ]   
+},{
+        
+        "name": "Ross",
+        "photo": "http://www.doozynews.com/wp-content/uploads/2015/01/14209374244110-14-times-ross-geller-proved-he-is-the-ultimate-ge-2-5800-1395344544-12_dblbig.jpg",
+        "scores": [
+            4,
+            2,
+            2,
+            1,
+            5,
+            1,
+            2,
+            4,
+            2
+        ]
+
+},{
+        "name": "Chandler",
+        "photo": "http://cimg.tvgcdn.net/i/2017/04/02/234bdf6a-e87a-41a8-94bf-07842011771d/6fc025cc00bfde6457f73440be21108a/chandler-friends-hp-lg.jpg",
+        "scores": [
+            3,
+            4,
+            5,
+            4,
+            3,
+            4,
+            5,
+            4,
+            2
+]
+}
+              ];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,12 @@ var path = require("path");
 //getting information from the friends.js file
 var friends = require("../data/friends.js");
 // Create New friends - takes in JSON input
+
+//the starting value of the calculated score
+var totalDiff = 0;
+//an array that holds all of the calculaed score.
+var calcDiff =[];
+
 module.exports = function (app) {
 
 // Get all friends information to show
@@ -15,10 +21,31 @@ module.exports = function (app) {
 app.post("/api/friends", function (req, res) {
 // req.body hosts is equal to the JSON post sent from the user
 var newFriends = req.body;
+//a loop going through the friends array
+    for(var i = 0; i < friends.length; i++){
+//inputing info through the function to calculate the difference in the user and each friends score. 
+        totalDifference(newFriends.scores,friends[i].scores);
+//making sure the calculation is working and is properly looping through each friends 
+        console.log(totalDiff);        
+//reseting the value to totalDiff to propery calculate the next friend in the friends array.
+        totalDiff = 0;    
+    }  
     
-// We then add the json the user sent to the friends array in the friends.js file.
+//function that goes through user scores and friends scores and calculate the difference.
+
+    function totalDifference(userScore,friends){
+//a loop going through just the scores of the friends and user scores.     
+        for(var j = 0; j< userScore.length; j++){
+//the calculation of the difference of the user and friends.
+        totalDiff += Math.abs(parseInt(userScore[j])- parseInt(friends[j]));
+        }
+        return totalDiff;
+        }
+
+// We then add the json the user sent to the friends array in the friends.js file.So it will show up in the /api/friends.
     friends.push(newFriends);
     
     
 })
+
 };
